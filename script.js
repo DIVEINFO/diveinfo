531235bbb7b839ac46a576c4bb4b089b4cc95413
DIVEINFO <blackbox01071994@gmail.com>
2026-02-11 10:13:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// MOBILE MENU
+function toggleMenu(){
+    const nav = document.getElementById("navMenu");
+    if(nav.style.display === "block"){
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
+
+// CAROUSEL
+let index = 0;
+const slides = document.getElementById("slideContainer");
+const totalSlides = slides.children.length;
+
+function showSlide(i){
+    if(i >= totalSlides) index = 0;
+    if(i < 0) index = totalSlides - 1;
+    slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+function moveSlide(step){
+    index += step;
+    showSlide(index);
+}
+
+function autoSlide(){
+    index++;
+    showSlide(index);
+}
+
+setInterval(autoSlide, 3000);
